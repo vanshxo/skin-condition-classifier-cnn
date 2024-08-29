206dd90d99429bfc569262ca2af71b901fbe4544
vanshxo <khatrivansh43@gmail.com>
2024-08-29 22:07:55 +0530

    structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is a deep Convolutional Neural Network for image classification of skin sample into 6 conditional catagories',
+    author='Vansh Khatri',
+    license='MIT',
+)
